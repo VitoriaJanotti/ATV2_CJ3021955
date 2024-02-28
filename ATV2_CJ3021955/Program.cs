e6b2b798a7e8d20f3ade55e47d27c1215a583a29
VitoriaJanotti <alcantara.maria@aluno.ifsp.edu.br>
2024-02-28 11:09:05 -0300

    fazendo exercício 5

--- ATV2_CJ3021955/Program.cs
+++ ATV2_CJ3021955/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            /*
+            //COMENTÁRIO DE LINHA ÚNICA 
+
             //ATIVIDADE 1 - SALA
 
             Console.WriteLine("ATIVIDADE1");
@@ -50,6 +53,7 @@
             soma = ant_d + suce_t;
 
             Console.WriteLine("Soma: ", soma);
+            */ 
 
             //EXERCÍCIO 1 - LISTA 
 
@@ -65,11 +69,58 @@
 
             // EXERCÍCIO 2 - LISTA 
 
-            float graus_f;
-            int graus_c;
+            float graus_f, graus_c;
+            int f, c;
 
             Console.WriteLine("Insira o valor em graus fahrenheit: ");
             graus_f = float.Parse(Console.ReadLine());
+            f = int.Parse(Console.ReadLine());
+
+            graus_c = (graus_f - 32.0f) * (5.0f / 9.0f);
+            c = (f - 32) * (5 / 9);
+
+            Console.WriteLine("graus celcius com float - {0} \n graus celcius com int - {1}", graus_c, c);
+            //O resultado dá 0 pois, a divisão 5/9 é inteira o que daria a dizima 0,5. como a variavel int não aceita números com virgulas seria igual a 0 e  quando se multiplica por 0 o resultado é 0.
+
+            //EXERCÍCIO 3 - LISTA 
+
+            float peso, altura, imc;
+
+            Console.WriteLine("Insira a sua altura em metros: ");
+            altura = float.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o seu peso em kilos: ");
+            peso = float.Parse(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+
+            Console.WriteLine("Seu IMC é: ", imc);
+
+            //EXERCÍCIO 4 - LISTA
+
+            float p1, p2, p3, media;
+
+            Console.WriteLine("Vamos calcular a média dos pesos");
+            Console.WriteLine("Insira o valor de p1: ");
+            p1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o valor de p2: ");
+            p2 = float.Parse(Console.ReadLine());   
+            Console.WriteLine("Insira o valor de p3: ");
+            p3 = float.Parse(Console.ReadLine());
+
+            media = (p1 + p2 + p3) / 3;
+
+            Console.WriteLine("A média dos pesos é: ", media);
+
+            //EXERCÍCIO 5 - LISTA 
+
+            Console.WriteLine(Vamos calcular a força elástica de uma mola)
+
+                //fel = k . x
+                // fel: força elástica(N)  k: constante elástica da mola(N/ m) /n x: deformação da mola(m)
+
+
+
+
 
         }
     }
